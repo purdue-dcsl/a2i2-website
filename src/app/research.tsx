a3203cb3f03ff51fe6d59b414d20fd59a79a5a66
Leon <leon.yee000@gmail.com>
2023-08-05 15:12:34 -0700

    research page done

--- src/app/research.tsx
+++ src/app/research.tsx
@@ -2,26 +2,26 @@
 
 import "./globals.css"
 
-import { useEffect, useState } from 'react'
 
 export default function Research() {
-    const [column, setImage] = useState({src: "/ancient-stone-column.png", hash: Date.now()});
-    useEffect(() => {
-        const theme = document.documentElement.getAttribute("data-theme");
-        if (theme === "dark") {
-            setImage({src: "/ancient-stone-column.png", hash: Date.now()});
-        }
-        else {
-            setImage({src: "/ancient-stone-column-dark.png", hash: Date.now()});
-        }
-    }, []);
 
     return (
-        <div className="p-12 w-screen lg:p-24 md:p-18">
-            <div className="flex flex-row">
-                <img src={`${column.src}?${column.hash}`} className="w-1/3" />
+        <div className="p-6 pt-0 w-screen lg:p-24 lg:pt-0 md:p-12 md:pt-0">
+            <div className="flex flex-row justify-between">
+                <img src='/ancient-stone-column-dark.png' className="w-1/3" />
+                <img src='/ancient-stone-column-dark.png' className="w-1/3" />
+                <img src='/ancient-stone-column-dark.png' className="w-1/3" />
             </div>
-            <p>kajskdjskjd</p>
+            <div className="flex flex-row justify-around">
+                <p className="w-1/3 px-5 text-center font-extrabold research-pillars-clamp">Robust adversarial algorithms</p>
+                <p className="w-1/3 px-5 text-center font-extrabold research-pillars-clamp">Interpretable algorithms aiding the trust of the warfighter on the results of the autonomous algorithms</p>
+                <p className="w-1/3 px-5 text-center font-extrabold research-pillars-clamp">Secure, distributed execution of the autonomy pipeline among multiple platforms</p>
+            </div>
+            <p className="mt-6 md:mt-16 description-clamp-2">The research contributions in these three pillars will combine to achieve the end deliverable of secure-by-design autonomous
+                algorithms that can operate under the constraints, and uncertainties, of a battlefield environment.
+                The instantiation will be in a prototype software system called SCRAMBLE
+                (SeCure Real-time Decision-Making for the AutonoMous BattLEfield).
+            </p>
         </div>
     )
 }
