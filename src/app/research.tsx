f19dbb78d54e480475c1be2e9aeee9d2a3d01c90
Leon <leon.yee000@gmail.com>
2023-08-12 16:08:19 -0700

    accents

--- src/app/research.tsx
+++ src/app/research.tsx
@@ -13,9 +13,9 @@ export default function Research() {
                 <img src='/ancient-stone-column-dark.png' className="w-1/3" />
             </div>
             <div className="flex flex-row justify-around">
-                <p className="w-1/3 px-5 text-center font-extrabold research-pillars-clamp">Robust adversarial algorithms</p>
-                <p className="w-1/3 px-5 text-center font-extrabold research-pillars-clamp">Interpretable algorithms aiding the trust of the warfighter on the results of the autonomous algorithms</p>
-                <p className="w-1/3 px-5 text-center font-extrabold research-pillars-clamp">Secure, distributed execution of the autonomy pipeline among multiple platforms</p>
+                <p className="w-1/3 px-5 text-center font-extrabold research-pillars-clamp"><span className="accent">Robust</span> adversarial algorithms</p>
+                <p className="w-1/3 px-5 text-center font-extrabold research-pillars-clamp"><span className="accent">Interpretable</span> algorithms aiding the trust of the warfighter on the results of the autonomous algorithms</p>
+                <p className="w-1/3 px-5 text-center font-extrabold research-pillars-clamp"><span className="accent">Secure</span>, distributed execution of the autonomy pipeline among multiple platforms</p>
             </div>
             <p className="mt-6 md:mt-16 description-clamp-2">The research contributions in these three pillars will combine to achieve the end deliverable of secure-by-design autonomous
                 algorithms that can operate under the constraints, and uncertainties, of a battlefield environment.
