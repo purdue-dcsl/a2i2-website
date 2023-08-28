48ff2661c2ff296eecde8e8ce7614fe7992135f1
Leon <leon.yee000@gmail.com>
2023-08-27 23:00:19 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: { ignoreBuildErrors: true, },
-  webpack: (config) => {
-    config.externals = {
-      canvas: "canvas",
-    };
-    return config;
-  },
-  typescript: { ignoreBuildErrors: true, },
 };
 
 module.exports = nextConfig
