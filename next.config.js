e2196db3039617135107a470c307ba4ef7b8a282
Leon <leon.yee000@gmail.com>
2023-08-27 23:19:05 -0400

    fix?

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.externals = {
+      canvas: "canvas",
+    };
+    return config;
+  },
   typescript: { ignoreBuildErrors: true, },
 };
 
