c0cb01f2d4c93fc052d02a0913b382e57000901c
Leon <leon.yee000@gmail.com>
2024-02-07 16:24:38 -0500

    Added Team Member

--- src/app/team.tsx
+++ src/app/team.tsx
@@ -5,7 +5,7 @@ import "./globals.css"
 export default function Team() {
 
     return (
-        <div className="p-3 md:p-12 w-screen lg:p-24 md:p-18">
+        <div className="p-3 md:p-12 w-screen lg:p-20">
             <div className="flex flex-wrap justify-around">
                 <div className="grid flex-col w-72 mb-5 place-content-center">
                     <img src='/saurabhbagchi.jpg' className="rounded-full drop-shadow-lg m-5 w-52 h-52 object-cover" />
@@ -58,6 +58,15 @@ export default function Team() {
                     <p className="text-center accent text-base">Security & privacy of ML<br />Security & privacy of<br />networked systems</p>
                     <a className="rounded-full contact-button text-base mt-2 w-9 h-9 text-center place-self-center hover:bg-gray-300"
                         href="mailto:pmittal@princeton.edu" target="_blank" rel="noopener noreferrer">
+                        <span className="grid h-full w-full place-content-center"><img src="/mail.svg" /></span>
+                    </a>
+                </div>
+                <div className="grid flex-col w-72 mb-5 place-content-center">
+                    <img src='/yinli.jpg' className="rounded-full drop-shadow-lg m-5 w-52 h-52 object-cover" />
+                    <p className="text-center text text-2xl">Yin Li</p>
+                    <p className="text-center accent text-base">Computer vision,<br />Automated image & video analysis</p>
+                    <a className="rounded-full contact-button text-base mt-2 w-9 h-9 text-center place-self-center hover:bg-gray-300"
+                        href="mailto:yin.li@wisc.edu" target="_blank" rel="noopener noreferrer">
                         <span className="grid h-full w-full place-content-center"><img src="/mail.svg" /></span>
                     </a>
                 </div>
