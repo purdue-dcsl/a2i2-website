69f008ac3dac222d996e32ad63c52a2cc6c0df07
Purdue's Dependable Computing Systems Lab <sbagchi@ecn.purdue.edu>
2024-06-27 17:56:44 -0400

    Added S&P vision paper

--- src/app/news.tsx
+++ src/app/news.tsx
@@ -8,6 +8,15 @@ export default function News() {
         <div className="p-6 pt-0 w-screen lg:p-24 lg:pt-0 md:p-12 md:pt-0">
             <div className="flex flex-wrap w-full mt-6">
                 <div className="lg:w-1/3 md:w-1/2 w-full hover:scale-105 transition-all">
+                    <a href="https://arxiv.org/abs/2309.01281" target="_blank" rel="noopener noreferrer" className="flex flex-col p-3 m-3 border">
+                        <img src="/gametheory-distsystems-vision.jpg" className="w-full object-cover h-48" />
+                        <p className="pt-4 font-normal text-xs">A2I2 researchers from Purdue and ARL lead a vision paper on game theory and distributed systems security to appear in IEEE Security and Privacy Magazine. </p>
+                        <p className="pt-1 text-sm">purdue.edu • Nov 2023</p>
+
+                    </a>
+            </div>
+            <div className="flex flex-wrap w-full mt-6">
+                <div className="lg:w-1/3 md:w-1/2 w-full hover:scale-105 transition-all">
                     <a href="https://engineering.purdue.edu/grandchallenges" target="_blank" rel="noopener noreferrer" className="flex flex-col p-3 m-3 border">
                         <img src="/news2.jpg" className="w-full object-cover h-48" />
                         <p className="pt-4 font-normal text-xs">ARL personnel play a leading role in the Grand Challenges in Resilience Workshop organized at Purdue November 13-15. ARL Branch Chief, Dr. Priya Narayanan, addressed the audience and ARL researcher, Dr. Peng Wang, judged the student poster competition. </p>
