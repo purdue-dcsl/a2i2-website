5f275adf39f64948f54b6d24f294218729b3ffaf
Leon <leon.yee000@gmail.com>
2024-06-28 10:29:31 -0700

    image fix

--- src/app/news.tsx
+++ src/app/news.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
+import Image from "next/image";
 import Papa from "papaparse";
 import "./globals.css"
 
@@ -31,9 +32,11 @@ export default function News() {
         <div className="p-6 pt-0 w-screen lg:p-24 lg:pt-0 md:p-12 md:pt-0">
             <div className="flex flex-wrap w-full mt-6">
                 {articles.map((article, index) => (
-                    <div className="lg:w-1/3 md:w-1/2 w-full hover:scale-105 transition-all">
+                    <div className="lg:w-1/3 md:w-1/2 w-full hover:scale-105 transition-all" key={index}>
                         <a href={article.link} target="_blank" rel="noopener noreferrer" className="flex flex-col p-3 m-3 border h-96">
-                            <img src={article.image} className="w-full object-cover h-48" />
+                            <div className="relative w-full h-48">
+                                <Image src={article.image} alt={article.title} fill className="object-cover"/>
+                            </div>
                             <p className="pt-2 font-bold text-lg overflow-scroll">{article.title}</p>
                             <p className="pt-1 text-gray-500 text-xs">{article.details}</p>
 
--- src/app/team.tsx
+++ src/app/team.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import "./globals.css"
+import Image from "next/image";
 
 export default function Team() {
 
@@ -8,68 +9,96 @@ export default function Team() {
         <div className="p-3 md:p-12 w-screen lg:p-20">
             <div className="flex flex-wrap justify-around">
                 <div className="grid flex-col w-72 mb-5 place-content-center">
-                    <a href="https://bagchi.github.io/" target="_blank" rel="noopener noreferrer"><img src='/saurabhbagchi.jpg' className="rounded-full drop-shadow-lg m-5 w-52 h-52 object-cover" /></a>
+                    <a href="https://bagchi.github.io/" target="_blank" rel="noopener noreferrer">
+                        <div className="drop-shadow-lg m-5 w-52 h-52">
+                            <Image src='/saurabhbagchi.jpg' alt="Saurabh Bagchi" className="rounded-full object-cover" fill />
+                        </div>
+                    </a>
                     <p className="text-center text text-2xl">Saurabh Bagchi</p>
                     <p className="text-center accent text-base">Distributed systems<br />System security and reliability</p>
                     <a className="rounded-full contact-button text-base mt-2 w-9 h-9 text-center place-self-center hover:bg-gray-300"
                         href="mailto:sbagchi@purdue.edu" target="_blank" rel="noopener noreferrer">
-                        <span className="grid h-full w-full place-content-center"><img src="/mail.svg" /></span>
+                        <span className="grid h-full w-full place-content-center"><img src="/mail.svg" alt="Mail" /></span>
                     </a>
                 </div>
                 <div className="grid flex-col w-72 mb-5 place-content-center">
-                    <a href="https://schaterji.io/" target="_blank" rel="noopener noreferrer"><img src='/somalichaterji.png' className="rounded-full drop-shadow-lg m-5 w-52 h-52 object-cover" /></a>
+                    <a href="https://schaterji.io/" target="_blank" rel="noopener noreferrer">
+                        <div className="drop-shadow-lg m-5 w-52 h-52">
+                            <Image src='/somalichaterji.png' alt="Somali Chaterji" className="rounded-full object-cover" fill />
+                        </div>
+                    </a>
                     <p className="text-center text text-2xl">Somali Chaterji</p>
                     <p className="text-center accent text-base">Distributed testbeds of<br />embedded & edge<br />Approximate computing</p>
                     <a className="rounded-full contact-button text-base mt-2 w-9 h-9 text-center place-self-center hover:bg-gray-300"
                         href="mailto:schaterji@purdue.edu" target="_blank" rel="noopener noreferrer">
-                        <span className="grid h-full w-full place-content-center"><img src="/mail.svg" /></span>
+                        <span className="grid h-full w-full place-content-center"><img src="/mail.svg" alt="Mail" /></span>
                     </a>
                 </div>
                 <div className="grid flex-col w-72 mb-5 place-content-center">
-                    <a href="https://www.purdue.edu/president/about/mung-chiang/" target="_blank" rel="noopener noreferrer"><img src='/mungchiang.jpg' className="rounded-full drop-shadow-lg m-5 w-52 h-52 object-cover" /></a>
+                    <a href="https://www.purdue.edu/president/about/mung-chiang/" target="_blank" rel="noopener noreferrer">
+                        <div className="drop-shadow-lg m-5 w-52 h-52">
+                            <Image src='/mungchiang.jpg' alt="Mung Chiang" className="rounded-full object-cover" fill />
+                        </div>
+                    </a>
                     <p className="text-center text text-2xl">Mung Chiang</p>
                     <p className="text-center accent text-base">Edge computing<br />Network optimization</p>
                     <a className="rounded-full contact-button text-base mt-2 w-9 h-9 text-center place-self-center hover:bg-gray-300"
                         href="mailto:chiang@purdue.edu" target="_blank" rel="noopener noreferrer">
-                        <span className="grid h-full w-full place-content-center"><img src="/mail.svg" /></span>
+                        <span className="grid h-full w-full place-content-center"><img src="/mail.svg" alt="Mail" /></span>
                     </a>
                 </div>
                 <div className="grid flex-col w-72 mb-5 place-content-center">
-                    <a href="https://kimkt.com/" target="_blank" rel="noopener noreferrer"><img src='/kwangtaikkim.jpg' className="rounded-full drop-shadow-lg m-5 w-52 h-52 object-cover" /></a>
+                    <a href="https://kimkt.com/" target="_blank" rel="noopener noreferrer">
+                        <div className="drop-shadow-lg m-5 w-52 h-52">
+                            <Image src='/kwangtaikkim.jpg' alt="Kwang Taik Kim" className="rounded-full object-cover" fill />
+                        </div>
+                    </a>
                     <p className="text-center text text-2xl">Kwang Taik Kim</p>
                     <p className="text-center accent text-base">Edge computing<br />NextG wireless</p>
                     <a className="rounded-full contact-button text-base mt-2 w-9 h-9 text-center place-self-center hover:bg-gray-300"
                         href="mailto:kimkt@purdue.edu" target="_blank" rel="noopener noreferrer">
-                        <span className="grid h-full w-full place-content-center"><img src="/mail.svg" /></span>
+                        <span className="grid h-full w-full place-content-center"><img src="/mail.svg" alt="Mail" /></span>
                     </a>
                 </div>
                 <div className="grid flex-col w-72 mb-5 place-content-center">
-                    <a href="https://www.davidinouye.com/" target="_blank" rel="noopener noreferrer"><img src='/davidinouye.jpg' className="rounded-full drop-shadow-lg m-5 w-52 h-52 object-cover" /></a>
+                    <a href="https://www.davidinouye.com/" target="_blank" rel="noopener noreferrer">
+                        <div className="drop-shadow-lg m-5 w-52 h-52">
+                            <Image src='/davidinouye.jpg' alt="David Inouye" className="rounded-full object-cover" fill />
+                        </div>
+                    </a>
                     <p className="text-center text text-2xl">David Inouye</p>
                     <p className="text-center accent text-base">Interpretable ML<br />Probabilistic models</p>
                     <a className="rounded-full contact-button text-base mt-2 w-9 h-9 text-center place-self-center hover:bg-gray-300"
                         href="mailto:dinouye@purdue.edu" target="_blank" rel="noopener noreferrer">
-                        <span className="grid h-full w-full place-content-center"><img src="/mail.svg" /></span>
+                        <span className="grid h-full w-full place-content-center"><img src="/mail.svg" alt="Mail" /></span>
                     </a>
                 </div>
                 <div className="grid flex-col w-72 mb-5 place-content-center">
-                    <a href="https://www.princeton.edu/~pmittal/" target="_blank" rel="noopener noreferrer"><img src='/prateekmittal.jpg' className="rounded-full drop-shadow-lg m-5 w-52 h-52 object-cover" /></a>
+                    <a href="https://www.princeton.edu/~pmittal/" target="_blank" rel="noopener noreferrer">
+                        <div className="drop-shadow-lg m-5 w-52 h-52">
+                            <Image src='/prateekmittal.jpg' alt="Prateek Mittal" className="rounded-full object-cover" fill />
+                        </div>
+                    </a>
                     <p className="text-center text text-2xl">Prateek Mittal</p>
                     <p className="text-center text text-xs">Princeton University</p>
                     <p className="text-center accent text-base">Security & privacy of ML<br />Security & privacy of<br />networked systems</p>
                     <a className="rounded-full contact-button text-base mt-2 w-9 h-9 text-center place-self-center hover:bg-gray-300"
                         href="mailto:pmittal@princeton.edu" target="_blank" rel="noopener noreferrer">
-                        <span className="grid h-full w-full place-content-center"><img src="/mail.svg" /></span>
+                        <span className="grid h-full w-full place-content-center"><img src="/mail.svg" alt="Mail" /></span>
                     </a>
                 </div>
                 <div className="grid flex-col w-72 mb-5 place-content-center">
-                    <a href="https://www.biostat.wisc.edu/~yli/" target="_blank" rel="noopener noreferrer"><img src='/yinli.jpg' className="rounded-full drop-shadow-lg m-5 w-52 h-52 object-cover" /></a>
+                    <a href="https://www.biostat.wisc.edu/~yli/" target="_blank" rel="noopener noreferrer">
+                        <div className="drop-shadow-lg m-5 w-52 h-52">
+                            <Image src='/yinli.jpg' alt="Yin Li" className="rounded-full object-cover" fill />
+                        </div>
+                    </a>
                     <p className="text-center text text-2xl">Yin Li</p>
                     <p className="text-center text text-xs">University of Wisconsin at Madison</p>
                     <p className="text-center accent text-base">Computer vision,<br />Automated image & video analysis</p>
                     <a className="rounded-full contact-button text-base mt-2 w-9 h-9 text-center place-self-center hover:bg-gray-300"
                         href="mailto:yin.li@wisc.edu" target="_blank" rel="noopener noreferrer">
-                        <span className="grid h-full w-full place-content-center"><img src="/mail.svg" /></span>
+                        <span className="grid h-full w-full place-content-center"><img src="/mail.svg" alt="Mail" /></span>
                     </a>
                 </div>
             </div>
@@ -78,35 +107,41 @@ export default function Team() {
             <h1 className="text-2xl font-bold border-b-4 border-yellow-500 w-fit">Army Research Laboratory (ARL)</h1>
             <div className="flex flex-wrap justify-around">
                 <div className="grid flex-col w-72 mb-5 place-content-center">
-                    <img src='/pengwang.jpg' className="rounded-full drop-shadow-lg m-5 w-52 h-52 object-cover" />
+                    <div className="drop-shadow-lg m-5 w-52 h-52">
+                        <Image src='/pengwang.jpg' alt="Peng Wang" className="rounded-full object-cover" fill />
+                    </div>
                     <p className="text-center text text-2xl">Peng Wang</p>
                     <p className="text-center accent text-base">Edge Computing<br />Network Optimization</p>
                     <div className="flex flex-row place-content-center gap-2">
                         <a className="rounded-full contact-button text-base mt-2 w-9 h-9 text-center place-self-center hover:bg-gray-300"
                             href="./bios/pengwang.html" target="_blank" rel="noopener noreferrer">
-                            <span className="grid h-full w-full place-content-center"><img src="/user.svg" /></span>
+                            <span className="grid h-full w-full place-content-center"><img src="/user.svg" alt="User" /></span>
                         </a>
                     </div>
                 </div>
                 <div className="grid flex-col w-72 mb-5 place-content-center">
-                    <img src='/jameshare.jpg' className="rounded-full drop-shadow-lg m-5 w-52 h-52 object-cover" />
+                    <div className="drop-shadow-lg m-5 w-52 h-52">
+                        <Image src='/jameshare.jpg' alt="Zach Hare" className="rounded-full object-cover" fill />
+                    </div>
                     <p className="text-center text text-2xl">Zach Hare</p>
                     <p className="text-center accent text-base">Statistical Inference<br />Uncertainty analysis<br />Reinforcement learning</p>
                     <div className="flex flex-row place-content-center gap-2">
                         <a className="rounded-full contact-button text-base mt-2 w-9 h-9 text-center place-self-center hover:bg-gray-300"
                             href="./bios/zachhare.html" target="_blank" rel="noopener noreferrer">
-                            <span className="grid h-full w-full place-content-center"><img src="/user.svg" /></span>
+                            <span className="grid h-full w-full place-content-center"><img src="/user.svg" alt="User" /></span>
                         </a>
                     </div>
                 </div>
                 <div className="grid flex-col w-72 mb-5 place-content-center">
-                    <img src='/kevinchan.jpg' className="rounded-full drop-shadow-lg m-5 w-52 h-52 object-cover" />
+                    <div className="drop-shadow-lg m-5 w-52 h-52">
+                        <Image src='/kevinchan.jpg' alt="Kevin Chan" className="rounded-full object-cover" fill />
+                    </div>
                     <p className="text-center text text-2xl">Kevin Chan</p>
                     <p className="text-center accent text-base">Edge computing<br />Wireless networks</p>
                     <div className="flex flex-row place-content-center gap-2">
                         <a className="rounded-full contact-button text-base mt-2 w-9 h-9 text-center place-self-center hover:bg-gray-300"
                             href="./bios/kevinchan.html" target="_blank" rel="noopener noreferrer">
-                            <span className="grid h-full w-full place-content-center"><img src="/user.svg" /></span>
+                            <span className="grid h-full w-full place-content-center"><img src="/user.svg" alt="User" /></span>
                         </a>
                     </div>
                 </div>
